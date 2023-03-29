d4439392b71d609b05ab8d41994769a00773b296
Nikita88575 <kekchekcebyrek@gmail.com>
2023-03-29 18:15:28 +0300

    rst

--- rst/rstpars.py
+++ rst/rstpars.py
@@ -16,13 +16,22 @@ async def rstua():
         async with session.get(url, headers=agent) as rp:
             r = await aiohttp.StreamReader.read(rp.content)
             html = BS(r, 'lxml')
-            aps = html.findAll('section', {'class': 'rst-ocb1'})
-
+            aps = html.find_all('div', {'class': 'rst-uix-margin'})
+            
             for ap in aps:
-                announcement1 = ap.find('div', {'class': 'rst-ocb-i rst-ocb-i-premium rst-uix-radius roiv rst-ocb-i-crash'}).find('a', {'class': 'rst-ocb-i-a'}).get('href')
-                announcement2 = ap.find('div', {'class': 'rst-ocb-i rst-ocb-i-premium rst-uix-radius roiv'})
-                announcement3 = ap.find('div', {'class': 'rst-ocb-i rst-ocb-i-premium rst-uix-radius roiv rst-ocb-i-blue'})
-                print(announcement1)
+                lst = ap.find('div', {'class': 'rst-uix-float-left'}).find_all('section', {'class': 'rst-ocb1'})
+                for l in lst:
+                    announcements = l.find('div', {'class': 'rst-page-wrap'}).find_all('div', {'class': 'rst-ocb-i rst-ocb-i-premium rst-uix-radius roiv'})
+                    for ann in announcements:
+                        link = ann.find('a', {'class': 'rst-ocb-i-a'}).get('href')
+                        title = ann.find('a', {'class': 'rst-ocb-i-a'}).find('h3', {'class': 'rst-ocb-i-h'}).find('span', {'class': ''}).text
+                    #prewiev_pic = announcements.find('a', {'class': 'rst-ocb-i-a'}).find('div', {'class': 'rst-ocb-i-i'}).find('img').get('src')
+                        print(f'Лінк: https://rst.ua{link}\nЗаголовок: {title}\n')
+            # announcements = aps.find('section', {'class': 'rst-ocb1'}).find('div', {'class': 'rst-ocb-i rst-ocb-i-premium rst-uix-radius roiv'})
+            # print(announcements)
+                # for rst in announcements:
+                #     rs = rst.find_all('div', {'class': 'rst-page-wrap'})
+                #     print(rs)
 
 
 
