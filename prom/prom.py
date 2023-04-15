974f1baf03fe4c90e265cd27f4a088abeb9565af
Nikita88575 <kekchekcebyrek@gmail.com>
2023-04-15 15:49:02 +0300

    made it better

--- prom/prom.py
+++ prom/prom.py
@@ -15,10 +15,12 @@ async def prom():
         lp = int(lp)
     except ValueError:
         sys.exit("Не коректне значення")
+
     for p in range(fp, lp + 1):
         print(f'Данні з {p} сторінки успішно завантаженні')
         url = f"https://prom.ua/ua/Noutbuki;{p}" 
         agent = {'User-Agent': UserAgent().random}
+
         async with aiohttp.ClientSession() as session:
             async with session.get(url, headers=agent) as rp:
                 r = await aiohttp.StreamReader.read(rp.content)
@@ -27,13 +29,16 @@ async def prom():
                 
                 for ap in aps:
                     link = ap.find('a', {'class': '_0cNvO jwtUM'}).get('href')
-                    title = ap.find('a', {'class': '_0cNvO jwtUM'}).get('title') #bkjEo
+                    title = ap.find('a', {'class': '_0cNvO jwtUM'}).get('title')
+
                     try:
                         price = ap.find('div', {'class', 'IP36L bkjEo'}).find('span').text
                     except Exception:
                         price = ap.find('div', {'class', 'bkjEo'}).find('span').text
+
                     shop_link = ap.find('div', {'class': 'M3v0L BXDW- qzGRQ aO9Co'}).find('a', {'class': '_0cNvO jwtUM'}).get('href')
                     shop_name = ap.find('div', {'class': 'M3v0L BXDW- qzGRQ aO9Co'}).find('a', {'class': '_0cNvO jwtUM'}).text
+
                     try:
                         shop_raiting = ap.find('div', {'class': 'M3v0L BXDW- qzGRQ aO9Co'}).find('span', {'class': '_0cNvO jwtUM OX5sJ XCtBJ'}).text
                     except Exception:
@@ -41,9 +46,9 @@ async def prom():
                             shop_raiting = ap.find('div', {'class': 'M3v0L BXDW- qzGRQ aO9Co'}).find('span', {'class': '_0cNvO jwtUM OX5sJ XCtBJ'}).text
                         except Exception:
                             shop_raiting = 'None'
-                    print(link, title, price, shop_link, shop_name, shop_raiting)
-                
+
                     data.append([f'https://prom.ua{link}', title, price, shop_link, shop_name, shop_raiting])
+
                 headers = ["Посилання", "Заголовок", "Ціна", "Лінк на магазин", "Ім'я магазину", "Рейтинг магазину"]    # список за яким будуть заповнені верхні колонки датафрейму
                 df = pandas.DataFrame(data, columns = headers)                          # конвертація масиву з даними у датафрейм       
                 df.to_csv('prom/your_table.csv')
