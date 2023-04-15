a25147d3a93378f7963b55d111f109eb0434c9dd
Nikita88575 <kekchekcebyrek@gmail.com>
2023-04-15 16:19:59 +0300

    made it better

--- autoria/autopars.py
+++ autoria/autopars.py
@@ -21,7 +21,6 @@ async def rstua():
         sys.exit("Не коректне значення")
     
     for p in range(fp, lp + 1):
-        print(f'Данні з {p} сторінки успішно завантаженні')
         url = f"https://auto.ria.com/uk/legkovie/ford/?page={p}.html"
         agent = {'User-Agent': UserAgent().random}
         async with aiohttp.ClientSession() as session:
@@ -33,19 +32,23 @@ async def rstua():
                 for ap in aps:
                     link = ap.find('a', {'class': 'm-link-ticket'}).get('href')
                     title = ap.find('a', {'class': 'address'}).get('title')
+
                     try:
                         priceUSD = ap.find('span', {'class': 'bold size22 green'}).text
                     except:
                         priceUSD = ap.find('span', {'class': 'bold green size22'}).text
+
                     priceUAH = ap.find('span', {'class': 'i-block'}).find('span', {'class': ''}).text
                     mileage = ap.find('li', {'class': 'item-char js-race'}).text
                     engine = ap.find_all('li', {'class': 'item-char'})[2].text
                     box = ap.find_all('li', {'class': 'item-char'})[3].text
+
                     data.append([link, title, priceUSD, priceUAH, mileage, engine, box])
+                print(f'Данні з {p} сторінки успішно завантаженні')
+
                 headers = ["Посилання", "Заголовок", "Ціна в $", "Ціна в грн", "Пробіг", "Двигун", "Коробка"]    # список за яким будуть заповнені верхні колонки датафрейму
                 df = pandas.DataFrame(data, columns = headers)                          # конвертація масиву з даними у датафрейм       
                 df.to_csv('autoria/your_table.csv')  
-
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
--- rieltor/main.py
+++ rieltor/main.py
@@ -17,15 +17,19 @@ async def domria():
     maxp = m.find('li', {'class': 'last'}).find('a', {'class': 'pager-btn'}).text               # ця конструкція дістає максимально доступний номер сторінки з сайту
     fp = input("Введіть номер сторінки з якої ви хочете почати: ")
     try:
+
         fp = int(fp)
     except ValueError:
         sys.exit("Число не підходить")
+    
     lp = input(f"Введіть номер сторінки на якій ви хочете завершити (не більше {maxp}): ")           # в цьому запиті при кожному старті програми підставляэться актуальна максимально доступна сторінка
     maxp = int(maxp)
+    
     try:
         lp = int(lp)
     except ValueError:
         sys.exit("Число не підходить")
+    
     if fp > 0 and lp <= maxp:
         if fp <= lp:
             print("Процес завантаження даних: ")
@@ -36,7 +40,6 @@ async def domria():
                         r = await aiohttp.StreamReader.read(rp.content)
                         html = BS(r, 'lxml')
                         aps = html.findAll('div', {'class': 'catalog-card'})
-                        print(p, "сторінка:")                                              # це я використав для того щоб корситувач бачив етап збору даних
 
                         for ap in aps:                                                     # цей цикл запбезпечує зміну оголошення
                             link = ap.find('a', {'class': 'catalog-card-media'}).get('href')              # змінна у яку зберігається посилання на оголошення
@@ -45,7 +48,10 @@ async def domria():
                             rooms = ap.find('div', {'class': 'catalog-card-details'}).findAll('span', {'class': ''})[0].text     # змінна у яку зберігається кількість кімнат
                             sizes = ap.find('div', {'class': 'catalog-card-details'}).findAll('span', {'class': ''})[1].text     # змінна у яку зберігаються площы квартири
                             flore = ap.find('div', {'class': 'catalog-card-details'}).findAll('span', {'class': ''})[2].text     # змінна у яку зберігається поверховість будівлі та поверх квартири
+                            
                             data.append([link, price, address, rooms, sizes, flore])        # у цей масив зберігаються всі дані для зручного виводу
+                        print(f'Данні з {p} сторінки успішно завантаженні')
+                            
                     headers = ["Посилання", "Ціна", "Адреса", "Кількість кімнат", "Площа", "Поверх"]    # список за яким будуть заповнені верхні колонки датафрейму
                     df = pandas.DataFrame(data, columns = headers)                          # конвертація масиву з даними у датафрейм       
                     df.to_csv('rieltor/your_table.csv')                                             # конвертація датафрейму у csv таблицю
